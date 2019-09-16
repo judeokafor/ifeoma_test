243af496047c9ab313befaf1791ec245b9d75eb3
Okafor Jude <Okaforjudechukwuebuka@gmail.com>
2019-09-16 15:50:55 +0100

    is admin using passpport authentication

--- server.js
+++ server.js
@@ -1,35 +1,42 @@
-//  server.js
-// const express = require('express')
-
+import User from './src/controllers/users';
+import Product from './src/controllers/products';
+import Sale from './src/controllers/sales';
+import isAdmin from './src/middleware/isAdmin';
 import express from 'express';
+import passport from 'passport';
 
-const app = express()
+const app = express();
 
-app.use(express.urlencoded({
-    extended: false
-}))
-app.use(express.json())
+app.use(passport.initialize());
+require('./src/passport/passport')(passport);
+app.use(
+	express.urlencoded({
+		extended: false,
+	})
+);
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    return res.status(200).send({
-        'message': 'Welcome!'
-    });
-})
-
-
-app.listen(5500)
-
-console.log('Server running on port', 5500);
-
-// import User from './src/controllers/users';
-import Product from './src/controllers/products';
-// import Sale from './src/controllers/sales';
-
-// app.post('/api/v1/Users', User.addPost);
-// app.get('/api/v1/Users', User.getAll);
-app.post('/api/v1/Products', Product.addPost);
+	return res.status(200).send({
+		message: 'Welcome!',
+	});
+});
+app.post('/api/v1/Users', User.addUser);
+app.get('/api/v1/Users', User.getAll);
+app.post(
+	'/api/v1/Products',
+	passport.authenticate('jwt', { session: false }),
+	isAdmin,
+	Product.addPost
+);
 app.get('/api/v1/Products', Product.getAll);
 app.get('/api/v1/Products/:id', Product.getOne);
-// app.post('/api/v1/Sales', Sale.addPost);
-// app.get('/api/v1/Sales', Sale.getAll);
-// app.get('/api/v1/Sales/:id', Sale.getOne);
+app.post('/api/v1/Sales', Sale.addPost);
+app.get('/api/v1/Sales', Sale.getAll);
+app.get('/api/v1/Sales/:id', Sale.getOne);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log(`Server Started at Port : ${PORT}`);
+});
--- src/models/Users.js
+++ src/models/Users.js
@@ -1,37 +1,49 @@
 import uuid from 'uuid';
+import jwt from 'jsonwebtoken';
 
 class User {
-    /**
-     * class constructor
-     * @param {object} data
-     */
-    constructor() {
-            this.users = [];
-        }
-        /**
-         * 
-         * @returns {object} user object
-         */
-    create(data) {
-        const newUser = {
-            id: uuid.v4(),
-            email: data.email || '',
-            role: data.role || '',
-            first_name: data.first_name || '',
-            mobile_number: data.mobile_number || '',
-            image_url: data.image_url || '',
-            sales: data.sales || ''
-        };
-        this.users.push(newUser);
-        return newUser
-    }
+	/**
+	 * class constructor
+	 * @param {object} data
+	 */
+	constructor() {
+		this.users = [];
+	}
+	/**
+	 *
+	 * @returns {object} user object
+	 */
+	create(data) {
+		const payload = {
+			email: data.email,
+			isAdmin: data.isAdmin,
+			role: data.role,
+			first_name: data.first_name,
+		};
+		const token = jwt.sign(payload, 'iliketocode');
+		const newUser = {
+			id: uuid.v4(),
+			email: data.email || '',
+			role: data.role || '',
+			first_name: data.first_name || '',
+			mobile_number: data.mobile_number || '',
+			image_url: data.image_url || '',
+			sales: data.sales || '',
+			isAdmin: data.isAdmin || false,
+			token: `Bearer ${token}` || '',
+		};
+		this.users.push(newUser);
+		return newUser;
+	}
 
-    /**
-     * @returns {object} returns all users
-     */
-    findAll() {
-        return this.users;
-    }
-
+	/**
+	 * @returns {object} returns all users
+	 */
+	findAll() {
+		return this.users;
+	}
+	findByEmail(email) {
+		return this.users.find(user => user.email === email);
+	}
 }
 export default new User();
